25c7db22a08b0b43e4dd84d9c29af2f65a635554
MarcLinsmeau <marc.linsmeau.pro@gmail.com>
2026-06-11 21:32:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -122,8 +122,8 @@ try:
         # 'index=[...]' crée les sous-groupements en lignes (Équipe > Joueur)
         # 'columns=[...]' crée les sous-groupements en colonnes (Année > Semaine)
         tcd_comptage = df_resultat.pivot_table(
-            index=["Equipe1", "Joueur1"],
-            columns=["Annee", "Semaine"],
+            index=["Equipe1", "Joueur1", "Annee", "Semaine"],
+            columns=["Match"],
             aggfunc="size",  # 'size' compte le nombre de lignes (matchs joués)
             fill_value=0     # Remplace les cases vides (où le joueur n'a pas joué) par un 0
         )
